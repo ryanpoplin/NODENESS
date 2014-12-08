ff1e4d1e3b6b7185d430e9a017b5028edb6cd52f
Byrdann Fox <mobileupstate@gmail.com>
2014-12-08 01:09:17 -0500

    Updates...

--- js/testness2.js
+++ js/testness2.js
@@ -2,6 +2,19 @@
 
     "use strict";
 
-    
+    var Readable = require('stream').Readable;
+    var readable = new Readable;
+    var count = 0;
+    // _read...?
+    readable._read = function() {
+        if (++count > 10) {
+            return readable.push(null);
+        }
+        setTimeout(function() {
+            readable.push(count + "\n");
+        }, 500);
+    };
+
+    readable.pipe(process.stdout);
 
 }());
