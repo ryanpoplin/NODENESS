a39d7b09f2436cdf464a36f3cd7d9cdd05262000
Byrdann Fox <mobileupstate@gmail.com>
2014-12-14 17:10:31 -0500

    Updates...

--- file-system/write-simple.js
+++ file-system/write-simple.js
@@ -3,11 +3,17 @@
 	"use strict";
 
 	const fs = require('fs');
+	
 	fs.writeFile('../text/text1.txt', 'a fucking message...', function(err) {
+		
 		if (err) {
+		
 			throw err;
+		
 		}
+		
 		console.log("File saved...");
+	
 	});
 
 }());
